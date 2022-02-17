935fbbef24417651cc12f4fbf6e39f19f4a0215c
lyltc1 <870767645@qq.com>
2022-02-17 15:08:38 +0000

    1

--- try/detectron2_try.py
+++ try/detectron2_try.py
@@ -63,35 +63,36 @@ def get_balloon_dicts(img_dir):
 
 for d in ["train", "val"]:
     DatasetCatalog.register("balloon_" + d, lambda d=d: get_balloon_dicts(
-        "/home/lyl/git/detectron2/datasets/balloon_dataset/balloon/" + d))
+        "/mnt1/linyongliang/detectron2/datasets/balloon/" + d))
     MetadataCatalog.get("balloon_" + d).set(thing_classes=["balloon"])
 balloon_metadata = MetadataCatalog.get("balloon_train")
 
-dataset_dicts = get_balloon_dicts("/home/lyl/git/detectron2/datasets/balloon_dataset/balloon/train")
-# for d in random.sample(dataset_dicts, 3):
-#     img = cv2.imread(d["file_name"])
-#     visualizer = Visualizer(img[:, :, ::-1], metadata=balloon_metadata, scale=0.5)
-#     out = visualizer.draw_dataset_dict(d)
-#     cv2.imshow("image", out.get_image()[:, :, ::-1])
-#     cv2.waitKey(0)
+dataset_dicts = get_balloon_dicts("/mnt1/linyongliang/detectron2/datasets/balloon/train")
+for d in random.sample(dataset_dicts, 1):
+    img = cv2.imread(d["file_name"])
+    visualizer = Visualizer(img[:, :, ::-1], metadata=balloon_metadata, scale=0.5)
+    out = visualizer.draw_dataset_dict(d)
+    cv2.imwrite("./image.png", out.get_image()[:, :, ::-1])
 
-from detectron2.engine import DefaultTrainer
-
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml"))
-cfg.DATASETS.TRAIN = ("balloon_train",)
-cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml")  # Let training initialize from model zoo
-cfg.SOLVER.IMS_PER_BATCH = 2
-cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-cfg.SOLVER.STEPS = []        # do not decay learning rate
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
-# NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
-
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg)
-trainer.resume_or_load(resume=False)
-trainer.train()
+#
+# from detectron2.engine import DefaultTrainer
+#
+# cfg = get_cfg()
+# cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+# cfg.DATASETS.TRAIN = ("balloon_train",)
+# cfg.DATASETS.TEST = ()
+# cfg.DATALOADER.NUM_WORKERS = 2
+# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+# cfg.SOLVER.IMS_PER_BATCH = 2
+# cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+# cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+# cfg.SOLVER.STEPS = []        # do not decay learning rate
+# cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+# cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon). (see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+# # NOTE: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+#
+# os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+# trainer = DefaultTrainer(cfg)
+# trainer.resume_or_load(resume=False)
+# trainer.train()
+#
